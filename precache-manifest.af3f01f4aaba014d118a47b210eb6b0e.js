637fd06b73fbdfbdc797eb63fbf7ef7ab765c2bd
anand ramnayan maurya <anandmr05@gmail.com>
2020-09-29 01:49:46 +0530

    Updates

---
+++ precache-manifest.af3f01f4aaba014d118a47b210eb6b0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005a0e7ae7e9551fcc66bc45f53c062c",
+    "url": "/ReactProject/index.html"
+  },
+  {
+    "revision": "e1afce61f2bfa6280567",
+    "url": "/ReactProject/static/css/main.2766b367.chunk.css"
+  },
+  {
+    "revision": "f1b7416415c87bb45b3d",
+    "url": "/ReactProject/static/js/2.1269889b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactProject/static/js/2.1269889b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1afce61f2bfa6280567",
+    "url": "/ReactProject/static/js/main.75f175e7.chunk.js"
+  },
+  {
+    "revision": "f85acedacc80f745f7c0",
+    "url": "/ReactProject/static/js/runtime-main.7d40a1a4.js"
+  }
+]);
